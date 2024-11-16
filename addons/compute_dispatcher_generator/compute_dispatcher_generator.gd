cfb7237ab178567afcf9d0f39a7a14940cb77b02
Michael Reel <mykilr@gmail.com>
2024-11-16 20:44:17 +0000

    Added the basic menu creation code

--- addons/compute_dispatcher_generator/compute_dispatcher_generator.gd
+++ addons/compute_dispatcher_generator/compute_dispatcher_generator.gd
@@ -2,11 +2,32 @@
 extends EditorPlugin
 
 
+var tool_menu_text: String = "Generate GLSL Compute Dispatcher..."
+
+
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
+	self.add_tool_menu_item(tool_menu_text, dispatcher_create)
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	self.remove_tool_menu_item(tool_menu_text)
+
+
+func dispatcher_create() -> void:
+	"""The main method for the dispatcher generator"""
+	
+	print_debug("dispatcher_create started")
+	
+	# Get the glsl file content
+	# parse for layout local size
+	# parse for each input and output
+	# create a script with:
+	# - some tool identifiable header
+	# - comment/doc indicating the file is generated and giving information
+	# - proper exports for setting up inputs
+	# - proper functions for extracting outputs
+	# - signals for completion
+	
+	print_debug("dispatcher_create complete")
